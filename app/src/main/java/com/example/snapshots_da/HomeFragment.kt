12923516c3edfb84f4488b6baffdc25af3f1836d
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-11-19 21:09:37 -0600

    Adapter: Sobrescritura de métodos

--- app/src/main/java/com/example/snapshots_da/HomeFragment.kt
+++ app/src/main/java/com/example/snapshots_da/HomeFragment.kt
@@ -1,16 +1,22 @@
 package com.example.snapshots_da
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.snapshots_da.databinding.FragmentHomeBinding
 import com.example.snapshots_da.databinding.ItemSnapshotBinding
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import java.math.MathContext
 
 class HomeFragment : Fragment() {
 
@@ -35,12 +41,38 @@ class HomeFragment : Fragment() {
             .setQuery(query, Snapshot::class.java).build()
 
         mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>(options){
+            private lateinit var mContext: Context
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+                mContext = parent.context
 
+                val view = LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_snapshot, parent, false)
+                return SnapshotHolder(view)
             }
 
             override fun onBindViewHolder(holder: SnapshotHolder, position: Int, model: Snapshot) {
+                var snapshot = getItem(position)
 
+                with(holder){
+                    setListener(snapshot)
+
+                    binding.tvTitle.text = snapshot.title
+                    Glide.with(mContext)
+                        .load(snapshot.photoUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                        .into(binding.imgPhoto)
+                }
+            }
+
+            override fun onDataChanged() {
+                super.onDataChanged()
+                mBinding.progressBar.visibility = View.GONE
+            }
+
+            override fun onError(error: DatabaseError) {
+                super.onError(error)
+                Toast.makeText(mContext,error.message,Toast.LENGTH_SHORT).show()
             }
         }
     }
