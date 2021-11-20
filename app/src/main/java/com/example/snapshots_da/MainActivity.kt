07ccc682892b3ad764a49d1c4f418acd5f851d5a
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-11-19 18:16:12 -0600

    Inicializar y configurar Fragments

--- app/src/main/java/com/example/snapshots_da/MainActivity.kt
+++ app/src/main/java/com/example/snapshots_da/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.snapshots_da
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.snapshots_da.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
+    
+    private lateinit var mActiveFragment: Fragment
+    private lateinit var mFraManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupBottomNav(){
-        val fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().add(R.id.hostFragment,HomeFragment()).commit()
+        mFraManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActiveFragment = homeFragment
+
+        mFraManager.beginTransaction()
+            .add(R.id.hostFragment,profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment).commit()
+        mFraManager.beginTransaction()
+            .add(R.id.hostFragment,addFragment, AddFragment::class.java.name)
+            .hide(addFragment).commit()
+        mFraManager.beginTransaction()
+            .add(R.id.hostFragment,homeFragment, HomeFragment::class.java.name).commit()
     }
 }
