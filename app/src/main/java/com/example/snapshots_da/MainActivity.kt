8ff25178013434fac73c35cf4db4ce0b5fd2ce6f
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-11-20 18:10:15 -0600

    Authentication: Mejoras

--- app/src/main/java/com/example/snapshots_da/MainActivity.kt
+++ app/src/main/java/com/example/snapshots_da/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.snapshots_da
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.snapshots_da.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import java.util.*
 
@@ -96,4 +99,18 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         mFirebaseAuth?.removeAuthStateListener(mAuthListener)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                Toast.makeText(this, "Bienvenido...",Toast.LENGTH_SHORT).show()
+            }else{
+                if (IdpResponse.fromResultIntent(data) == null){
+                    finish()
+                }
+            }
+        }
+    }
+
 }
